7191cef63ecb7ba6be41bf6b134c92944ec226f2
KitchenGun <kitchengun124@gmail.com>
2020-01-21 13:26:04 +0900

    Initial commit

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,30 @@
+﻿
+#include <iostream>
+using namespace std;
+
+//class ClassName 
+//{
+	//접근 권한 지정자를 생략하면 private권한으로 취급한다
+	//private
+	//public
+	//protected
+	//맴버 변수
+	//맴버 함수
+//};
+
+class Person
+{
+//private:
+public:
+	char name[11];
+	int age;
+
+};
+
+void main()
+{
+	Person kang;
+	//클래스 객체 멤버에 접근하는 방법
+	strcpy_s(kang.name,"kang");
+    cout << kang.name; 
+}
